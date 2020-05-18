a911c27f2bc633ed6a803f247b5190404b23c491
adrian61 <ad.kowalskI@gmail.com>
2020-05-18 22:43:35 +0200

    Test lazy loading after close session.

--- src/main/java/org/example/EagerLazyDemo.java
+++ src/main/java/org/example/EagerLazyDemo.java
@@ -26,10 +26,15 @@ public class EagerLazyDemo {
 			// console if lazy
 			// Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, instructor1_.id as id1_2_1_, instructor1_.hobby as hobby2_2_1_, instructor1_.youtube_channel as youtube_3_2_1_ from instructor instructor0_ left outer join instructor_detail instructor1_ on instructor0_.instructor_detail_id=instructor1_.id where instructor0_.id=?
 
-			System.out.println(tempInstructor.getCourseList());
+			//System.out.println(tempInstructor.getCourseList());
 			// added if lazy
 			//Hibernate: select courselist0_.instructor_id as instruct3_0_0_, courselist0_.id as id1_0_0_, courselist0_.id as id1_0_1_, courselist0_.instructor_id as instruct3_0_1_, courselist0_.title as title2_0_1_ from course courselist0_ where courselist0_.instructor_id=?
+
 			session.getTransaction().commit();
+
+			// Uncomment to test lazy loading after close session.
+			System.out.println(tempInstructor.getCourseList());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
